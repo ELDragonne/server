c943a184aa01b8abce8e5725efcc4cd6fa15b440
FORESTIERK <ForestierCODAGE@outlook.fr>
2017-06-04 18:17:45 +0200

    server

---
+++ workshop.lua
@@ -0,0 +1,218 @@
+﻿resource.AddWorkshop("672009017") -- Alucard Playermodel
+resource.AddWorkshop("456964786") -- Assassins Creed: Altair Ibn-La'Ahad Playermodel
+resource.AddWorkshop("294159280") -- Black Knight (Dark Souls) Player Model
+resource.AddWorkshop("320793184") -- Black Legion Cultist Playermodel & NPC
+resource.AddWorkshop("105117556") -- ColorChangeable Geth Playermodel
+resource.AddWorkshop("153105840") -- Dark Khan Playermodel
+resource.AddWorkshop("495367178") -- Dark Souls: Artorias
+resource.AddWorkshop("485469106") -- Dark Souls: Painting Guardian
+resource.AddWorkshop("526052354") -- Darth Maul Playermodel
+resource.AddWorkshop("821743621") -- Deathwing - PlayerModel | NPC | Ragdoll - [Warcraft]
+resource.AddWorkshop("813688302") -- Demonic Pact Anubis Playermodel Pack (Smite)
+resource.AddWorkshop("650914151") -- Dracula Playermodel
+resource.AddWorkshop("624332876") -- EWR-115 Playermodel/NPC's
+resource.AddWorkshop("832439743") -- Evie Frye - Assassins Creed Syndicate [PM]
+resource.AddWorkshop("105683141") -- FFVII Cloud Playermodel
+resource.AddWorkshop("403994254") -- Gabranth Playermodel
+resource.AddWorkshop("451758808") -- Game of Thrones - Iron Throne
+resource.AddWorkshop("601509912") -- Jedi BattleLord Armor Pack : SWTOR
+resource.AddWorkshop("760941737") -- Doomsday Playermodel (Injustice)
+resource.AddWorkshop("235244321") -- Divii's Waframe Ember Prime PlayerModel
+resource.AddWorkshop("733835039") -- Guts Playermodel [Berserk]
+resource.AddWorkshop("788109554") -- Gandalf Player and NPC
+resource.AddWorkshop("842951069") -- Grand Inquisitor Temple Guard
+resource.AddWorkshop("600150713") -- Kotor Pack : Knight of the Old Republic PlayerModels
+resource.AddWorkshop("110871780") -- Hit Numbers
+resource.AddWorkshop("631940768") -- Kathryn Playermodel
+resource.AddWorkshop("704211282") -- Light Saber for Yutori SandBox CONTENT
+resource.AddWorkshop("668456592") -- LOL | Syndra | NPC + Playermodel
+resource.AddWorkshop("221904792") -- Mass Effect - Geth Playermodel
+resource.AddWorkshop("156318356") -- Lord of the Rings Player Models
+resource.AddWorkshop("222287348") -- Mass Effect - Legion Playermodel
+resource.AddWorkshop("115368733") -- Mass Effect Props
+resource.AddWorkshop("712419489") -- Lana Beniko
+resource.AddWorkshop("420436409") -- Mass Effect Props - Part Four
+resource.AddWorkshop("420381877") -- Mass Effect Props - Part One
+resource.AddWorkshop("420390352") -- Mass Effect Props - Part Two
+resource.AddWorkshop("420433780") -- Mass Effect Props - Part Three
+resource.AddWorkshop("257102395") -- Metal Gear Raiden Player Model Pack
+resource.AddWorkshop("600130097") -- Satele Shan : SWTOR
+resource.AddWorkshop("400684744") -- Monster Hunter Playermodel
+resource.AddWorkshop("790028133") -- PPAP Sound Gun
+resource.AddWorkshop("546377707") -- John Cena Playermodel
+resource.AddWorkshop("867016380") -- MH16: God Emperor Doom (PM and Ragdoll)
+resource.AddWorkshop("803397031") -- MGSV: Cyborg Ninja Playermodel
+resource.AddWorkshop("726069482") -- OW- Reaper Playermodel
+resource.AddWorkshop("516377504") -- Roll Command
+resource.AddWorkshop("154033711") -- Jin Roh Playermodel
+resource.AddWorkshop("857988405") -- Ryte Playermodel
+resource.AddWorkshop("685691024") -- Satan Playermodel
+resource.AddWorkshop("284266415") -- Sci-fi Props Megapack
+resource.AddWorkshop("792308926") -- Smite | Sun Wukong | NPC + Playermodel
+resource.AddWorkshop("316735786") -- SSBB Ganondorf Playermodel
+resource.AddWorkshop("865435880") -- Star Wars - More Jedi Species Playermodel Pack v1.0
+resource.AddWorkshop("666417939") -- Star Wars: Jedi Youngling Playermodels
+resource.AddWorkshop("846489399") -- [World of Warcraft] Dark Ranger Anya Playermodel
+resource.AddWorkshop("865262802") -- [Vindictus] Elculous Dragon Playermodel
+resource.AddWorkshop("836398959") -- [TES:V] Hagraven Playermodel
+resource.AddWorkshop("827985614") -- [TES:V] Ebony Warrior (Armor) Playermodel
+resource.AddWorkshop("835592400") -- [TES:V] Dragon Priest Playermodel
+resource.AddWorkshop("827187732") -- [TES:V] Argonian Playermodel
+resource.AddWorkshop("852192700") -- [Skyrim] Nightingale Playermodel & Ragdoll
+resource.AddWorkshop("867264214") -- [Skyrim] Dark Elf Playermodel & Ragdoll
+resource.AddWorkshop("104482086") -- [Official] Precision Tool
+resource.AddWorkshop("317172786") -- Xianghua Chai (Playermodel)
+resource.AddWorkshop("694799274") -- [FSAS] Star Wars Darth Revan Variants
+resource.AddWorkshop("835089874") -- [Daggerdale] Dragonborn Playermodel (Dragonbone)
+resource.AddWorkshop("638229249") -- White Rock Shooter (Digitrevx) Playermodel and NPC
+resource.AddWorkshop("251316439") -- Warframe Tenno Pack: Boyframes
+resource.AddWorkshop("272148309") -- Verdugo Playermodel
+resource.AddWorkshop("108176967") -- The sit anywhere script!
+resource.AddWorkshop("157373328") -- Tyrael playermodel
+resource.AddWorkshop("794538126") -- Thanatos Playermodel (Smite)
+resource.AddWorkshop("339693062") -- Stella Bullet model
+resource.AddWorkshop("242939488") -- Strelok Playermodel
+resource.AddWorkshop("175697854") -- The Elder Scrolls V Skyrim: Daedric Playermodel
+resource.AddWorkshop("668109580") -- Star Wars: Quinlan Vos Playermodel
+resource.AddWorkshop("645610156") -- Star Wars: Qui-Gon Jinn Playermodel
+resource.AddWorkshop("674455546") -- Star Wars: Mara Jade Skywalker Playermodel
+resource.AddWorkshop("832352895") -- SW:TOR Master Orgus Din Playermodel
+resource.AddWorkshop("852894538") -- SW:TOR Arcann Playermodel
+resource.AddWorkshop("600596739") -- Starkiller : TFU2 Kamino PlayerModel
+resource.AddWorkshop("654158454") -- Star Wars: Sith Assassin Playermodel
+resource.AddWorkshop("728461320") -- StarWars - Starkiller/Galen Marek Playermodels
+resource.AddWorkshop("707742612") -- Starlord Guardians of the Galaxy Player Model
+resource.AddWorkshop("643453713") -- Star Wars: The Props Awaken
+resource.AddWorkshop("654489982") -- Star Wars: Four Jedi PlayerModel Pack
+resource.AddWorkshop("832309946") -- Star Wars Battlefront: Elite Squadron - X1 Sith Lord
+resource.AddWorkshop("495762961") -- Star Wars Vehicles: Episode 1
+resource.AddWorkshop("608632308") -- Star Wars Vehicles: Episode 2
+resource.AddWorkshop("628120855") -- Star Wars Flags & Banners
+resource.AddWorkshop("780889405") -- Star Wars Jedi Knight Jedi Academy Cultist Playermodel
+resource.AddWorkshop("655780248") -- Star Wars: Darth Savet Playermodel
+resource.AddWorkshop("784459655") -- Star Wars: Cybernetic Starkiller
+resource.AddWorkshop("791688382") -- Star Wars TFU Sith Acolyte
+resource.AddWorkshop("855631618") -- Star Wars The Old Republic - Massive Prop Pack (278 PROPS!)
+resource.AddWorkshop("668821867") -- Star Wars: EG-5 Jedi Hunter Droid [PM/NPC/RAG]
+resource.AddWorkshop("109643223") -- 3D2D Textscreens
+resource.AddWorkshop("518960557") -- Sith & Jedi Playermodel Pack
+resource.AddWorkshop("604533553") -- Shaak Ti Playermodel/NPCs
+resource.AddWorkshop("438641850") -- Geth Playermodels
+resource.AddWorkshop("526087892") -- Dark/Light Revan Player Model REUPLOAD
+resource.AddWorkshop("207948202") -- Simple ThirdPerson - Sliders & Fixes!
+resource.AddWorkshop("106798118") -- Revan Dark Lightsaber Pack
+resource.AddWorkshop("601241666") -- Revan : SWTOR
+resource.AddWorkshop("141400305") -- KOTOR Models
+resource.AddWorkshop("109645302") -- Light Revan
+resource.AddWorkshop("159269477") -- Corvo playermodel
+resource.AddWorkshop("204105025") -- Connect/Disconnect Messages
+resource.AddWorkshop("691971740") -- CorruptedNinja's Custom Playermodel
+resource.AddWorkshop("115753588") -- Fading door tool
+resource.AddWorkshop("606378269") -- Darth Jadus : SWTOR
+resource.AddWorkshop("602618106") -- Darth Malgus : SWTOR
+resource.AddWorkshop("600577642") -- Darth Marr : SWTOR
+resource.AddWorkshop("303831756") -- Darth Nihilus Playermodel
+resource.AddWorkshop("109144876") -- Darth Nihilus
+resource.AddWorkshop("600664109") -- Eternal Pack : SWTOR
+resource.AddWorkshop("109644453") -- Darth Revan
+resource.AddWorkshop("603884889") -- Darth Sion : TFU 2
+resource.AddWorkshop("386572373") -- Dark Souls: Ciaran
+resource.AddWorkshop("370820630") -- Dark Souls: Darkwraith
+resource.AddWorkshop("484058515") -- Dark Souls: Knight Kirk
+resource.AddWorkshop("303424179") -- Darth/Light Revan Playermodels
+resource.AddWorkshop("542890380") -- Dead or Alive 5 - Momiji (Player Model & NPC)
+resource.AddWorkshop("602424721") -- GRSW : Resources #2
+resource.AddWorkshop("625190722") -- GRSW : Resources #3
+resource.AddWorkshop("612569962") -- GRSW#2 | Model Content
+resource.AddWorkshop("618175188") -- GRSW#3 : servercontent#4
+resource.AddWorkshop("617921341") -- GRSW#3: Model Content
+resource.AddWorkshop("618041956") -- GRSW#4: Model Content
+resource.AddWorkshop("598363236") -- GRSW:Resources
+resource.AddWorkshop("471562011") -- Injustice Gods Among Us Scorpion Playermodel/Npc
+resource.AddWorkshop("625885529") -- Kasumi Goto [player/npc]
+resource.AddWorkshop("674629681") -- Jedi Academy - Dual Lightsabers
+resource.AddWorkshop("229976936") -- Join, Connect, and Disconnect Messages
+resource.AddWorkshop("123846159") -- Lightsaber Model Pack [FULL] *NEW*
+resource.AddWorkshop("107087351") -- Mileena npc & playermodel
+resource.AddWorkshop("464136465") -- MK Noob Npc/playermodel
+resource.AddWorkshop("468527669") -- MK Reptile Npc/playermodel
+resource.AddWorkshop("462829327") -- MK Skarlet Npc/playermodel
+resource.AddWorkshop("468972568") -- MKX Cassie Cage Npc/Playermodel
+resource.AddWorkshop("471093063") -- MKX Ermac Npc/playermodel
+resource.AddWorkshop("471561812") -- MKX Erron Black Npc/playermodel
+resource.AddWorkshop("470054104") -- MKX Johnny Cage Npc/playermodel
+resource.AddWorkshop("473426738") -- MKX Kenshi Npc/playermodel
+resource.AddWorkshop("581722338") -- MKX Kenshi Playermodel Recompressed
+resource.AddWorkshop("472651378") -- MKX Raiden Npc/playermodel
+resource.AddWorkshop("471430600") -- MKX Rain Npc/playermodel
+resource.AddWorkshop("471891866") -- MKX Sonya Blade Npc/playermodel
+resource.AddWorkshop("472207620") -- MKX Tanya Npc/playermodel
+resource.AddWorkshop("154034027") -- Moon Knight Playermodel
+resource.AddWorkshop("612987657") -- My Character from Star Wars: The Old Republic
+resource.AddWorkshop("119003252") -- Mortal Kombat 9 - ShangTsung_
+resource.AddWorkshop("337233095") -- Mortal Kombat 9 - Sub-Zero
+resource.AddWorkshop("337141378") -- Mortal Kombat 9 - Reptile
+resource.AddWorkshop("488122182") -- RemoveProps
+resource.AddWorkshop("223357888") -- Rage - Enforcer Playermodel and Ragdoll
+resource.AddWorkshop("602616761") -- Rahm Kota : TFU
+resource.AddWorkshop("131729710") -- Plo Koon
+resource.AddWorkshop("889397486") -- Shinnok Playermodel - Mortal Kombat X
+resource.AddWorkshop("125490679") -- Player Join/Leave Tag
+resource.AddWorkshop("220336312") -- PermaProps
+resource.AddWorkshop("105920435") -- Model Manipulator
+resource.AddWorkshop("113147611") -- Star Wars Imperial Melee
+resource.AddWorkshop("734483032") -- Star Wars: Maulkiller Playermodel
+resource.AddWorkshop("779894902") -- Starkiller (Cybernetic Reconstruction)
+resource.AddWorkshop("768960778") -- Star Wars : Lightsabers Pack
+resource.AddWorkshop("852238599") -- SW:TOR Thexan Model
+resource.AddWorkshop("863722945") -- Star Wars: The Darth Jar Jar Binks theory is partially true
+resource.AddWorkshop("894487689") -- Grey Force Wardens
+resource.AddWorkshop("876356702") -- [Apex] Star Wars: Kinect - Jedi & Sith Playermodels (7)
+resource.AddWorkshop("721438820") -- Tyzen Xebec
+resource.AddWorkshop("664941368") -- Star Wars Enhanced Lightsabers
+resource.AddWorkshop("637501166") -- The Grand Inquisitor Playermodel
+resource.AddWorkshop("168889997") -- The Meteor Storm
+resource.AddWorkshop("480482198") -- Batman Arkham Knight Azrael Playermodel/Npc
+resource.AddWorkshop("467171487") -- Batman Arkham City Azrael Playermodel/Npc
+resource.AddWorkshop("542191624") -- Shaak T'I TFU
+resource.AddWorkshop("760699025") -- Hyrule Warriors Twili Midna playermodel
+resource.AddWorkshop("639756713") -- Balthazar Outfit Playermodel (Guild Wars 2)
+resource.AddWorkshop("263446966") -- Cry of Fear Simon Playermodel (half-dead)
+resource.AddWorkshop("154032871") -- Darkseid Playermodel
+resource.AddWorkshop("857468373") -- Darsenvall's Lightsabers System [Content only]
+resource.AddWorkshop("834259345") -- Fixed Fallout Playermodels
+resource.AddWorkshop("158864234") -- MGS - Gray Fox Playermodel
+resource.AddWorkshop("775578849") -- Red death ncr ranger (Playermodel)
+resource.AddWorkshop("599429860") -- Animus [STAR WARS RP] Sith Pack
+resource.AddWorkshop("431813760") -- Assassin's Creed Chronicles China: Shao Jun Playermodel
+resource.AddWorkshop("237984526") -- Assassins Creed II: Ezio Auditore Playermodel
+resource.AddWorkshop("238213159") -- Assassins Creed IV: Edward Kenway Playermodel
+resource.AddWorkshop("442214334") -- Assassins Creed Rogue: Shay Cormac Playermodel
+resource.AddWorkshop("453667254") -- MKX: Sub-Zero P.M.
+resource.AddWorkshop("423474482") -- Halo 4 Ur-Didact Player/Npc
+resource.AddWorkshop("889159258") -- SWTOR: Shilen
+resource.AddWorkshop("604379289") -- [SWTOR] Star Wars : The Old Republic Music Soundtrack - Part 01
+resource.AddWorkshop("604380460") -- [SWTOR] Star Wars : The Old Republic Music Soundtrack - Part 02
+resource.AddWorkshop("604381760") -- [SWTOR] Star Wars : The Old Republic Music Soundtrack - Part 03
+resource.AddWorkshop("604382977") -- [SWTOR] Star Wars : The Old Republic Music Soundtrack - Part 04
+resource.AddWorkshop("878165847") -- [Vindictus] Cromm Cruaich Playermodel
+resource.AddWorkshop("472078854") -- MKX: Kotal Kahn P.M and Ragdoll
+resource.AddWorkshop("454787966") -- MKX: Scorpion P.M. and Ragdoll
+resource.AddWorkshop("475477215") -- MKX: Ermac P.M. and Ragdoll
+resource.AddWorkshop("471450333") -- MKX: Kitana P.M. and Ragdoll
+resource.AddWorkshop("461667700") -- MKX: Mileena P.M. and Ragdoll
+resource.AddWorkshop("470790433") -- MKX: Reptile P.M. and Ragdoll
+resource.AddWorkshop("469260198") -- MKX: Smoke P.M. and Ragdoll
+resource.AddWorkshop("662671797") -- Asajj Ventress : Clone Wars
+resource.AddWorkshop("582985454") -- Starkiller Sith Stalker Armor Playermodel
+resource.AddWorkshop("805799312") -- Star Wars Imperial Player Models
+resource.AddWorkshop("871488065") -- Star Wars: The Old Republic Darth Baras Playermodel
+resource.AddWorkshop("540636207") -- Star Wars: The Old Republic Playermodels & NPCs Sandbox Adaption (Playermodels Broken)
+resource.AddWorkshop("871404225") -- Star Wars: The Old Republic Senya Tirall Playermodel
+resource.AddWorkshop("390320635") -- Stacker v2 (Anti Crash)
+resource.AddWorkshop("868022323") -- [UR] Starwars: The Old Republic Playermodels
+resource.AddWorkshop("857292787") -- [UR] Starwars: The Old Republic Playermodels #1
+resource.AddWorkshop("868838596") -- [UR] Starwars Playermodels
+resource.AddWorkshop("803929070") -- SW:TOR Tulak Hord Playermodel
+resource.AddWorkshop("738616649") -- Star Wars : Imperial Map
+resource.AddWorkshop("767486604") -- [CS] LightSaber Content
